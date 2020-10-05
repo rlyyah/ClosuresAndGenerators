ffcb064e99981fed9ec361d8fda8cafcbddc7c40
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-10-05 13:43:27 +0200

    Making rules function into generator

--- ClosuresAndGenerators.py
+++ ClosuresAndGenerators.py
@@ -167,4 +167,24 @@ fib_list = list(fib(10000))
 print(fib_list)
 
 
+def rules_new(rules_filename):
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            patter, search, replace = line.split(None, 2)
+            yield build_match_apply_functions(pattern, search, replace)
+
+def plural_new(noun, rules_filename ='plural4-rules.txt'):
+    for matches_rule, apply_rule in rules_new(rules_filename):
+        if matches_rule(noun):
+            return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
+
+'''
+With generators, you can do everything lazily: 
+you read the first rule and create functions and try them, 
+and if that works you don’t ever read the rest of the file 
+or create any other functions.
+'''
+
+
 
