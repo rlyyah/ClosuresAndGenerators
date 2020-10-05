444194da56ee84a84a0be0cd02cd4361e58c6473
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-10-05 12:49:40 +0200

    Makign pattern and rules for closers, mapping them and then building function that uses them

--- ClosuresAndGenerators.py
+++ ClosuresAndGenerators.py
@@ -66,9 +66,36 @@ def plural(noun):
 
 print(plural('fly'))
 
+'''
+Function that builds other function dynamically
+Technique of using the values of outside parameters within 
+a dynamic function is called 'closures'
+'''
+
 def build_match_apply_functions(pattern, search, replace):
     def matches_rule(word):
         return re.search(pattern, word)
     def apply_rule(word):
         return re.sub(search, replace, word)
     return (matches_rule, apply_rule)
+
+patterns = \
+    (
+        ('[sxz]$', '$', 'es'),
+        ('[^aeioudgkprt]h$', '$', 'es'),
+        ('(qu|[^aeiou]y$)', 'y$', 'ies'),
+        ('$', '$', 's')
+    )
+rules = [build_match_apply_functions(pattern, search, replace) for
+        (pattern, search, replace) in patterns]
+
+def make_word_plural(word):
+    for matches_rule, apply_rule in rules:
+        if matches_rule(word):
+            return apply_rule(word)
+
+some_non_plural_words = ['fly', 'dog', 'cat', 'car', 'hero', 'sex']
+
+for word in some_non_plural_words:
+    print(word, ' -> ', make_word_plural(word))
+
